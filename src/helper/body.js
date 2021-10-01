847f9c1b09ff1a97992628c0b79619a821e6a9f0
coderwelsch <social@coderwelsch.com>
2021-10-01 17:16:29 +0200

    Changed body email texts

---
+++ src/helper/body.js
@@ -0,0 +1,8 @@
+
+const body = `Name: IHR_NAME
+Adresse: IHRE_ADRESSE
+Telefon (für Rückfragen): TELEFON
+
+Ihre Nachricht ❤️ …`;
+
+export default body
--- src/helper/index.js
+++ src/helper/index.js
@@ -1,6 +1,9 @@
+import body from "./body.js" ;
+
+
+
 function getMailLink () {
     const subject = `👋 Ahoi! – Anmeldung Bürger:Beete`;
-    const body = `Name: IHR_NAME\r\nAdresse: IHRE_ADRESSE\r\n`;
 
     return `mailto:info@buerger-beete.de?subject=${ encodeURIComponent(subject) }&body=${ encodeURIComponent(body) }`;
 }
